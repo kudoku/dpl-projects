9082546ec617a3046dee23aa779091c645e7cf31
Joon <kudo@Joons-MacBook-Air.local>
2014-11-26 13:49:24 -0700

    update routes

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'say/hello' => 'say#hello'
 
-  get 'say/goodbye​' => 'say#goodbye​'
+  get 'say/goodbye' => 'say#goodbye'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
